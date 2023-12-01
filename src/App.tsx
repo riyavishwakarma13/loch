1e0a3e76fd8afccd5f369847bb39b41592243a46
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2023-12-01 15:47:05 +0530

    sign up half done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import SignUp from "./components/SignUp";
+
 function App() {
-  return <div>fdgdkgfdjlgkf</div>;
+  return (
+    <div className="radial-gradient flex items-center">
+      <div className=" flex-1">other div</div>
+      <div className="">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,24 @@
+const SignUp = () => {
+  return (
+    <div className="bg-white flex flex-col items-center justify-center gap-5">
+      <h1 className="font-semibold text-4xl text-gray-400">
+        Sign up <br /> for exclusive access.
+      </h1>
+      <form className="flex flex-col gap-5 w-full max-w-[340px]">
+        <input
+          type="text"
+          placeholder="Your email address"
+          className="border border-gray-300 py-5 px-6 rounded-lg shadow-md placeholder:text-gray-300"
+        />
+        <button className="bg-black  text-white font-semibold py-4 px-7 rounded-lg">
+          Get Started
+        </button>
+      </form>
+      <p className="font-semibold">
+        You&apos;ll receive an email with an invite link to join.
+      </p>
+    </div>
+  );
+};
+
+export default SignUp;
