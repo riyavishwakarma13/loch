6725a8458bbdbcb8bdb4d40695bc91f819ea94b4
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2023-12-01 19:19:56 +0530

    testmonials done

--- src/components/Section/3.tsx
+++ src/components/Section/3.tsx
@@ -6,7 +6,7 @@ const Three = () => {
       <div className="pb-6 mr-20 border-b border-[#E5E5E680]">
         <h4 className="text-white text-2xl text-right">Testimonials</h4>
       </div>
-      <div className="flex flex-wrap items-end py-5 justify-between gap-6">
+      <div className="flex flex-col lg:flex-row items-end py-5 justify-between gap-6">
         <img src="/assets/Vector.svg" alt="Loch Logo" height={60} width={60} />
         <TestimonialSwiper />
       </div>
--- src/components/TestimonialSwiper.tsx
+++ src/components/TestimonialSwiper.tsx
@@ -8,80 +8,47 @@ const TestimonialSwiper = () => {
       <Swiper
         slidesPerView={"auto"}
         spaceBetween={30}
+        roundLengths={true}
+        loop={true}
         pagination={{
           clickable: true,
         }}
-        className="mySwiper "
+        className="mySwiper pr-3"
       >
-        <SwiperSlide className="bg-white w-[190px] rounded-lg box-gradient space-y-3 h-[171px] p-3">
-          <div className="flex justify-between items-start">
-            <img
-              src="/assets/bell 2.svg"
-              height={28}
-              width={28}
-              alt="bell carousel"
-            />
-            <p className="font-semibold text-[9px]">Save</p>
+        <SwiperSlide className="bg-white w-full max-w-[350px] rounded-xl box-gradient space-y-3 p-4">
+          <div className="flex items-center gap-2">
+            <p className="text-base font-semibold">Jack F</p>
+            <p className="text-sm text-gray-400 font-medium">Ex Blackrock PM</p>
           </div>
-          <p className="text-sm font-medium">
-            We&apos;ll be sending notifications to you <br /> here
-          </p>
-          <p className="text-xs p-2 bg-white border border-gray-200 rounded-md">
-            hello@gmail.com
+          <p>
+            “Love how Loch integrates portfolio analytics and whale watching
+            into one unified app.”
           </p>
         </SwiperSlide>
-        <SwiperSlide className="bg-white w-[190px] rounded-lg flex flex-col justify-between h-[171px] p-3">
-          <div className="flex justify-between items-start">
-            <img
-              src="/assets/bar-chart-2.svg"
-              alt="bar chart"
-              height={28}
-              width={28}
-            />
-            <img
-              src="/assets/icon.svg"
-              alt="checkbox"
-              className="bg-[#0071E3] rounded-[4.2px] p-[3px]"
-              height={16}
-              width={16}
-            />
-          </div>
-          <div className="space-y-3">
-            <p className="text-xs font-medium">
-              Notify me when any wallets move more than
+        <SwiperSlide className="bg-white w-full max-w-[350px] rounded-xl box-gradient space-y-3 p-4">
+          <div className="flex items-center gap-2">
+            <p className="text-base font-semibold">Yash P</p>
+            <p className="text-sm text-gray-400 font-medium">
+              Research, 3poch Crypto Hedge Fund
             </p>
-            <div className="bg-[#e5e5e680] flex items-center gap-1 w-max p-1 rounded-sm">
-              <span className="text-[9px] font-medium">$1,000.00</span>
-              <img src="/assets/triangle-down.svg" alt="" />
-            </div>
           </div>
+          <p>
+            “I use Loch everyday now. I don't think I could analyze crypto whale
+            trends markets without it. I'm addicted!”
+          </p>
         </SwiperSlide>
-        <SwiperSlide className="bg-white w-[190px] rounded-lg flex flex-col justify-between h-[171px] p-3">
-          <div className="flex justify-between items-start">
-            <img
-              src="/assets/clock.svg"
-              alt="bar chart"
-              height={28}
-              width={28}
-            />
-            <img
-              src="/assets/icon.svg"
-              alt="checkbox"
-              className="bg-[#0071E3] rounded-[4.2px] p-[3px]"
-              height={16}
-              width={16}
-            />
-          </div>
-          <div className="space-y-1">
-            <p className="text-xs font-medium">
-              Notify me when any wallet dormant for
+        <SwiperSlide className="bg-white w-full max-w-[530px] rounded-xl box-gradient space-y-3 p-4">
+          <div className="flex items-center gap-2">
+            <p className="text-base font-semibold">Shiv S</p>
+            <p className="text-sm text-gray-400 font-medium">
+              Co-Founder Magik Labs
             </p>
-            <div className="bg-[#e5e5e680] flex items-center gap-1 w-max p-1 rounded-sm">
-              <span className="text-[9px] font-medium">&gt; 30 days</span>
-              <img src="/assets/triangle-down.svg" alt="" />
-            </div>
-            <p className="text-xs font-medium">becomes active</p>
           </div>
+          <p>
+            “Managing my own portfolio is helpful and well designed. What&apos;s
+            really interesting is watching the whales though. No one else has
+            made whale tracking so simple.”
+          </p>
         </SwiperSlide>
       </Swiper>
     </div>
