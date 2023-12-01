c6b04b119c88a93346d3ad10b7667fb5b7bd7685
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2023-12-01 17:05:12 +0530

    notification swiper completed

--- src/components/NotificationSwiper.tsx
+++ src/components/NotificationSwiper.tsx
@@ -4,17 +4,17 @@ import "swiper/css/pagination";
 
 const NotificationSwiper = () => {
   return (
-    <div>
+    <div className="max-w-[400px] lg:max-w-[600px] px-5">
       <Swiper
         slidesPerView={"auto"}
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        className="mySwiper"
+        className="mySwiper "
       >
         <SwiperSlide className="bg-white w-[190px] rounded-lg box-gradient space-y-3 h-[171px] p-3">
-          <div className="flex justify-between items-start  ">
+          <div className="flex justify-between items-start">
             <img
               src="/assets/bell 2.svg"
               height={28}
@@ -30,8 +30,59 @@ const NotificationSwiper = () => {
             hello@gmail.com
           </p>
         </SwiperSlide>
-        <SwiperSlide className="bg-white">Slide 2</SwiperSlide>
-        <SwiperSlide className="bg-white">Slide 3</SwiperSlide>
+        <SwiperSlide className="bg-white w-[190px] rounded-lg flex flex-col justify-between h-[171px] p-3">
+          <div className="flex justify-between items-start">
+            <img
+              src="/assets/bar-chart-2.svg"
+              alt="bar chart"
+              height={28}
+              width={28}
+            />
+            <img
+              src="/assets/icon.svg"
+              alt="checkbox"
+              className="bg-[#0071E3] rounded-[4.2px] p-[3px]"
+              height={16}
+              width={16}
+            />
+          </div>
+          <div className="space-y-3">
+            <p className="text-xs font-medium">
+              Notify me when any wallets move more than
+            </p>
+            <div className="bg-[#e5e5e680] flex items-center gap-1 w-max p-1 rounded-sm">
+              <span className="text-[9px] font-medium">$1,000.00</span>
+              <img src="/assets/triangle-down.svg" alt="" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="bg-white w-[190px] rounded-lg flex flex-col justify-between h-[171px] p-3">
+          <div className="flex justify-between items-start">
+            <img
+              src="/assets/clock.svg"
+              alt="bar chart"
+              height={28}
+              width={28}
+            />
+            <img
+              src="/assets/icon.svg"
+              alt="checkbox"
+              className="bg-[#0071E3] rounded-[4.2px] p-[3px]"
+              height={16}
+              width={16}
+            />
+          </div>
+          <div className="space-y-1">
+            <p className="text-xs font-medium">
+              Notify me when any wallet dormant for
+            </p>
+            <div className="bg-[#e5e5e680] flex items-center gap-1 w-max p-1 rounded-sm">
+              <span className="text-[9px] font-medium">&gt; 30 days</span>
+              <img src="/assets/triangle-down.svg" alt="" />
+            </div>
+            <p className="text-xs font-medium">becomes active</p>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
